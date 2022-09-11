ba1c390d7ddda23e0109711654d6f12b97f3f6ef
godseun_mac <alcy1024@gmail.com>
2022-09-12 00:34:42 +0900

    swagger added

---
+++ src/main/java/com/mate/carpool/config/SwaggerAPIConfig.java
@@ -0,0 +1,28 @@
+package com.mate.carpool.config;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerAPIConfig {
+
+  @Bean
+  public GroupedOpenApi publicApi() {
+    return GroupedOpenApi.builder()
+        .group("v1-definition")
+        .pathsToMatch("/api/**")
+        .build();
+  }
+
+  @Bean
+  public OpenAPI springShopOpenAPI() {
+    return new OpenAPI()
+        .info(new Info().title("Mate 6.0 Car Pool API")
+            .description("Spring server application")
+            .version("v0.0.1"));
+  }
+}
