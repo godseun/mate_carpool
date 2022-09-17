aac96af526e155a06e8a9934fd237b9b91acf7aa
godseun_mac <alcy1024@gmail.com>
2022-09-18 03:56:17 +0900

    test add

--- src/main/java/com/mate/carpool/controller/TestController.java
+++ src/main/java/com/mate/carpool/controller/TestController.java
@@ -1,8 +1,12 @@
 package com.mate.carpool.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mate.carpool.dto.TestDTO;
 
 @RestController
 @RequestMapping("test")
@@ -11,5 +15,10 @@ public class TestController {
   @GetMapping
   public String findAll() {
     return "Hello Mate Team";
+  }
+
+  @PostMapping
+  public String jsonTest(@RequestBody TestDTO testDTO) {
+    return testDTO.toString();
   }
 }
