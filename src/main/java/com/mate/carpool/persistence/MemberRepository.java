c22cf32663eae4205eb156d60c6ae0f8516ad9e7
godseun_mac <alcy1024@gmail.com>
2022-09-19 09:42:19 +0900

    clear import

--- src/main/java/com/mate/carpool/persistence/MemberRepository.java
+++ src/main/java/com/mate/carpool/persistence/MemberRepository.java
@@ -2,15 +2,11 @@ package com.mate.carpool.persistence;
 
 import com.mate.carpool.model.MemberEntity;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
-
-  // Optional<MemberEntity> findByMemberId(Long memberId);
 
   MemberEntity findByEmail(String email);
 
